3a25ea255019414ca37b8399bffc89794cf4645f
liyang1221 <19198926@qq.com>
2014-10-23 09:50:19 +0800

    -update

--- Source/iReader/BookList.cs
+++ Source/iReader/BookList.cs
@@ -38,12 +38,31 @@ namespace iReader
             System.Diagnostics.Debug.WriteLine("fuck ... " + _cache.Cache.Length);
             var block = 550;
             var index = 0;
+			var next = 0;
             while(index < _cache.Cache.Length)
             {
                 var length = index + block > _cache.Cache.Length ? _cache.Cache.Length - index : block;
-                _list.Add(_cache.Cache.ToString(index, length));
-                index += length;
+				next = FindNewLinePosition(_cache.Cache, index + length);
+				if (next > 0)
+				{
+					_list.Add(_cache.Cache.ToString(index, next - index));
+					index = next;
+				}
+				else
+				{
+					_list.Add(_cache.Cache.ToString(index, length));
+					index += length;
+				}
             }
         }
+
+		int FindNewLinePosition(StringBuilder text, int start = 0)
+		{
+			for (int i = start; i < text.Length; i++)
+			{
+				if (text[i] == '\n') return i + 1;
+			}
+			return -1;
+		}
     }
 }
